cb2bbda823bfe12980c4964afca88fdbc8f27f7f
cjdummer10 <cjdummer10@gmail.com>
2022-03-24 17:38:31 -0400

    the button works
    ^^^

--- IFISH_game/Assets/Scripts/MainMenu.cs
+++ IFISH_game/Assets/Scripts/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
  
     public void startGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        SceneManager.LoadScene("SampleScene");
     }
 
 }
