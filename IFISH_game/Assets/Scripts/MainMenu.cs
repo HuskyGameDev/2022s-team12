fb8da39b8ff35c015e1632182666665cd5dfe972
cjdummer10 <cjdummer10@gmail.com>
2022-03-24 14:29:21 -0400

    Buttons
    slightly messing around with buttons, specifically an exit button on the game scene, not the menu scene

--- IFISH_game/Assets/Scripts/MainMenu.cs
+++ IFISH_game/Assets/Scripts/MainMenu.cs
@@ -6,8 +6,9 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
  
- public void startGame(){
-    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
-}
+    public void startGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
 
 }
