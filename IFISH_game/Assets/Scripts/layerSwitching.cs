95fbf664c4b8bdb42808a3e63d048b4c4fc32b07
cjdummer10 <cjdummer10@gmail.com>
2022-04-04 17:42:05 -0400

    dynamic layering
    added a script to dynamic change the layer of objects relative to the player so that they appear in front of or behind as appropriate
    changed filtering type on sprites to be less blurry

--- IFISH_game/Assets/Scripts/layerSwitching.cs
+++ IFISH_game/Assets/Scripts/layerSwitching.cs
@@ -7,15 +7,25 @@ public class layerSwitching : MonoBehaviour
 
     public Transform obj;
     public Transform player;
+    public SpriteRenderer objR;
+    public float offset;
 
 
     // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     void FixedUpdate()
     {
-       if(obj.position.y < player.position.y)
-        {
-
-        }
+       if(obj.position.y < player.position.y + offset )
+       {
+            objR.sortingLayerName = "ObjectsBelow";
+       }
+       if(obj.position.y > player.position.y + offset )
+       {
+            objR.sortingLayerName = "ObjectsAbove";
+       }
     }
 }
